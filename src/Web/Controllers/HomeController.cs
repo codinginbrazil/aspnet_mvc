88e30f3742d2385a3161194be1cf1f92caf02c0a
sswellington <sswellington@pm.me>
2023-06-27 00:39:10 -0300

    chore: rm logger

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -5,13 +5,6 @@ using System.Diagnostics;
 namespace Web.Controllers;
 public class HomeController : Controller
 {
-	private readonly ILogger<HomeController> _logger;
-
-	public HomeController(ILogger<HomeController> logger)
-	{
-		_logger = logger;
-	}
-
 	public IActionResult Index()
 	{
 		return View();
